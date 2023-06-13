1526af02fdc183f730e96afe8cf26aec49f1d280
kkunal <kkunal@mathworks.com>
2023-06-13 11:32:43 +0530

    Ajax Request
    Async Request - AJAX using XMLHTTPRequest

---
+++ DAY2/AJAX/GetData.js
@@ -0,0 +1,18 @@
+function GetData() {
+    console.log("Ajax logic here");
+    // 1. Instantiate XMLHttpRequest.
+    // 2. Open a connection.
+    // 3. Register on an event - onreadystatechange
+    // 4. XMLHttpRequest.send()
+
+    // The browser provides this method to us
+    let xMLHttpRequest = new XMLHttpRequest();
+    //https://jsonplaceholder.typicode.com -  TEST
+    xMLHttpRequest.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xMLHttpRequest.onreadystatechange = function() {
+        if(xMLHttpRequest.readyState == 4 && xMLHttpRequest.status ==200){
+            console.log(xMLHttpRequest.responseText);
+        }
+    };
+        xMLHttpRequest.send(); // places the async request - same as setTimeout
+    }
